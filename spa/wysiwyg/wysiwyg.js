c10fc3984a0be8b34d22076dde91518ff1fb3c64
Dylan Thomas <dylanthomas@Dylans-MacBook-Pro-2.local>
2016-02-18 15:21:02 -0600

    redid exercise in jquery

--- spa/wysiwyg/wysiwyg.js
+++ spa/wysiwyg/wysiwyg.js
@@ -15,7 +15,7 @@ var famousPeople = [
 	  image: "http://a5.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTIwNjA4NjM0MTMxMDg4OTA4.jpg",
 	  lifespan: {
 	    birth: 1962
-	  }		
+	  }
 	},
 	{
 	  title: "Rock Climber",
@@ -23,58 +23,45 @@ var famousPeople = [
 	  bio: "Dean Potter was an American free climber, alpinist, BASE jumper, BASEliner, and highliner. He was noted for hard first ascents, free solo ascents, speed ascents, and enchainments in Yosemite National Park and Patagonia.",
 	  image: "http://www.trbimg.com/img-555a49b2/turbine/la-me-ln-extreme-athlete-dean-potter-dead-yosemite-2015051",
 	  lifespan: {
-	    birth: 1972, 
+	    birth: 1972,
 	    death: 2015
-	  }		
-	},	
+	  }
+	},
 ]
 
-var container = document.getElementById("container");
-var textInput = document.getElementById("text-input");
-
-// create a div and output to the DOM for each person in the array
-famousPeople.forEach(function(currentPerson) {
-	var elementContent = "";
-	elementContent += `<div class="person"><person>`;	
-	elementContent += `<header>${currentPerson.name}: ${currentPerson.title}</header>`; 
-	elementContent += `<section><p>${currentPerson.bio}</p>`;
-	elementContent += `<img src=${currentPerson.image}></section>`;
-	if (currentPerson.lifespan.death) {
-		elementContent += `<footer>${currentPerson.lifespan.birth} to ${currentPerson.lifespan.death}</footer></person>`;		
-	} else {
-	elementContent += `<footer>${currentPerson.lifespan.birth} to present</footer></person></div>`;
+// add to DOM
+$(famousPeople).each(function(i, el) {
+	if (!el.lifespan.death) {
+		el.lifespan.death = "present";
 	}
-	container.innerHTML += elementContent;
+	$('#container').append(`
+		<div class="person"><person>
+		<header>${el.name}: ${el.title}</header>
+		<section><p>${el.bio}</p>
+		<img src=${el.image}></section>
+		<footer>${el.lifespan.birth} to ${el.lifespan.death}</footer>
+		</person></div>`);
 });
 
-// reference all divs with class of person
-var person = document.getElementsByClassName("person");
-
-// loop through the divs
-for (var i = 0; i < person.length; i++) {
-	var currentPerson = person[i];
-	// if you click a person,
-	currentPerson.addEventListener("click", function(){
-		console.log("this", this);
-		console.log("currentPerson", currentPerson);
-		// turn a class of "border" on or off
-		this.classList.toggle('border');
-		// focus on text input area
-		textInput.focus();
-		// this refers to <p>bio</p> inside div > person > section
-		var bioToReplace = this.childNodes[0].childNodes[1].childNodes[0];
-		// causes person's bio to change based on text input, or clear input if enter key is pressed 
-		textInput.addEventListener("keyup", function() {
-			if (event.keyCode === 13) {
-				textInput.value = "";
-			}
-			else {
-				var newContent = textInput.value;
-				bioToReplace.innerHTML = newContent;
-			}
-		});
+var bioToReplace;
+// event listeners
+$('.person').each(function(i, currPerson) {
+	$(currPerson).on('click', function(event) {
+		var personDiv = event.currentTarget;
+		$(personDiv).addClass('border');
+		$(personDiv).siblings().removeClass('border');
+		$('#text-input').focus();
+		bioToReplace = $(personDiv).find('p');
+		console.log(bioToReplace);
 	});
-}
-
-	
+	$('#text-input').on("keyup", function(event) {
+		if (event.which === 13) {
+			$('#text-input').val('');
+		}
+		else {
+			var newContent = $(this).val();
+			$(bioToReplace).html(newContent);
+		}
+	});
+});
 
