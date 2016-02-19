943ca2af614b1733b2976618d6897ffa0a373a50
Dylan Thomas <kdylanthomas@gmail.com>
2016-02-19 15:33:52 -0600

    refactored using jquery and es6

--- spa/seasonal-sales/seasonal-sales.js
+++ spa/seasonal-sales/seasonal-sales.js
@@ -1,94 +1,64 @@
-// webpage pulls in two files using XHR 
+"use strict";
+// webpage pulls in two files using XHR
 // for each product card: product, dept, price
 // select element that immediately applies seasonal discounts
 
-var container = document.getElementById("product-container");
-var discountSelects = document.getElementById("season-discounts");
-
 // declare these variables in global scope for use in updateDOM()--update their values inside loader functions
-var categoriesData;
-var productsData;
-var categories;
-var products;
-
-// make categories XHR in global scope 
-var categoryRequest = new XMLHttpRequest;
-categoryRequest.addEventListener("load", createCategoriesObject);
-categoryRequest.open("GET", "categories.json");
-categoryRequest.send();
+let categoriesData;
+let productsData;
+let categories;
+let products;
 
 
-function createCategoriesObject() {
-	console.log("createCategoriesObject");
-	categoriesData = JSON.parse(this.responseText);
+$.ajax({
+	url: 'categories.json'
+}).done(createCategoriesObject);
+
+function createCategoriesObject(categoriesData) {
 	categories = categoriesData.categories;
-	console.log("categoriesData", categoriesData);
-	console.log("categories", categories);
+	$.ajax({
+		url: 'products.json'
+	}).done(createProductsObject);
 
-	var productRequest = new XMLHttpRequest;
-	productRequest.addEventListener("load", createProductsObject);
-	productRequest.open("GET", "products.json");
-	productRequest.send();
-
-
-	function createProductsObject() {
-		console.log("createProductsObject");
-		productsData = JSON.parse(this.responseText);
+	function createProductsObject(productsData) {
 		products = productsData.products;
-		console.log("productsData", productsData);
-		console.log("products", products);
-
-		// event listener goes here
-		discountSelects.addEventListener("change", updateDOM);
+		$('#season-discounts').on('change', updateDOM);
 		updateDOM();
 	}
 }
 
 
 function updateDOM() {
-	var cardContents = "";
-	for (var i = 0; i < products.length; i++) {
-		var currentProduct = products[i];
-		// add product name to card
+	let cardContents = "";
+
+	$(products).each((i, currentProduct) => {
+		console.log(currentProduct);
 		cardContents += `<div class='card col-md-3'><h2>${currentProduct.name}</h2>`;
-		// add department name to card
-		for (var j = 0;  j < categories.length; j++) {
-			var currentCategory = categories[j];
-			// if the product's category ID matches the current category
+		$(categories).each((j, currentCategory) => {
 			if (currentProduct.category_id === currentCategory.id) {
-				// add the category name to the div 
 				cardContents += `<h5>${categories[j].name}</h5>`;
-				// add price to card
-				// if the season the user selects matches the current category's season for discount
-				if (discountSelects.value === currentCategory.season_discount) {
-					console.log(currentCategory.season_discount);
-					// store percentage rate for discount in var discountRate
-					var discountRate = currentCategory.discount;
+				if (($('#season-discounts').val() === currentCategory.season_discount)) {
+					let discountRate = currentCategory.discount;
 					// calculate a discounted price
-					var discountedPrice = currentProduct.price - (currentProduct.price * discountRate);
+					let discountedPrice = currentProduct.price - (currentProduct.price * discountRate);
 					discountedPrice = discountedPrice.toFixed(2);
-					// add discounted price to page
 					cardContents += `<h5 class="discounted">$${discountedPrice}</h5>`;
-				}
-				else {
-					// add normal price to page
+				} else {
 					cardContents += `<h5>$${currentProduct.price}</h5>`;
 				}
 				cardContents += `<p><a href="#" class="btn btn-primary" role="button">Add To Cart</a></p></div>`;
 			}
-		}
-	}
-	container.innerHTML = cardContents;
+		});
+	});
+	$('#product-container').html(cardContents);
 
 	// adds sale class to sale items when selected
-	var cards = document.getElementsByClassName("card");
-	console.log(cards[2]);
-	for (var i = 0; i < cards.length; i++) {
-		var searchForSale = cards[i].innerHTML;
+	$('.card').each((i, currCard) => {
+		let searchForSale = $(currCard).html();
 		if (searchForSale.indexOf("discounted") !== -1) {
-			cards[i].classList.add('sale');
+			$(currCard).addClass('sale');
 		}
-	}
+	});
 }
 
 
