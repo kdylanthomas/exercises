5581bbaf89198d55f7ede379f717f6ab27d4b49e
Dylan Thomas <dylanthomas@Dylans-MacBook-Pro-2.local>
2016-02-26 11:44:41 -0600

    finished refactoring, added select button

--- mjd/acme-explosives/app.js
+++ mjd/acme-explosives/app.js
@@ -3,17 +3,17 @@
 let categoryToLoad;
 let catIdToSearch;
 let typeIdToSearch;
+let output = "";
 
-
-$('#select').on('change', function() {
+// listen for button click
+$('#search').on('click', function () {
 	$('#results').html('');
+	output = "";
 	categoryToLoad = $('#select').val();
-	console.log("cat to load", categoryToLoad);
 	sortProducts();
 })
 
-// refactoring
-
+// find products that match user select; call buildHTML on those products
 function sortProducts() {
 	$(categories).each((i, currCat) => {
 		if (currCat.name === categoryToLoad) {
@@ -32,27 +32,24 @@ function sortProducts() {
 	});
 }
 
-
-
+// create HTML string and output to DOM
 function buildHTML (type, prod, desc, count) {
-	let output = "";
+	$('#results').html('');
 	if (categoryToLoad === "Demolition") {
 		count -= 9;
 	}
-	console.log(count);
-	// if (count % 3 === 0 || count === 0) {
-	// 	output += `<div class="row">`;
-	// }
-	output += `<div class="col-md-4 product-card">
+	if (count % 3 === 0) {
+		output += `<div class="row">`;
+	}
+	output += `<div class="col-md-4 product-card" id=${count}>
 		<p>Type: ${type}</p>
 		<p>Product: ${prod}</p>
 		<p>${desc}</p>
 	</div>`;
-	if (count % 3 === 0)  {
-		let rowID = count + "";
-		// output += `</div>`;
+	if ((count + 1) % 3 === 0)  {
+		output += `</div>`;
+		$('#results').html(output);
 	}
-	$('#results').append(output);
 }
 
 
