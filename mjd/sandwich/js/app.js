e2edbf6ab7dcaf3fcabd6052cd10390a5ac1db46
Dylan Thomas <kdylanthomas@gmail.com>
2016-02-26 14:29:33 -0600

    turned dropdowns into checkboxes

--- mjd/sandwich/js/app.js
+++ mjd/sandwich/js/app.js
@@ -6,61 +6,49 @@ let selectedToppings = "";
 let selectedBread;
 let selectedMeat;
 let selectedCheese;
-let selectedVeggie;
+let selectedVeggies;
 let selectedCondiment;
 
 console.log("Sandwich Maker", SandwichMaker);
-// console.log("add topping", addTopping);
 
-$('#bread').on('change', function(e) {
-	$('#bread').children().each((i, option) => {
-		if (option.selected === true) {
-			selectedBread = $(option).val();
-		}
-	})
-	console.log("you selected", selectedBread);
-	SandwichMaker.bread(selectedBread);
-	selectedToppings += `<li>${selectedBread}</li>`;
+$('.bread').each((i, option) => {
+	$(option).on('click', function() {
+		selectedBread = $(option).val();
+		SandwichMaker.bread(selectedBread);
+		selectedToppings += `<li>${selectedBread}</li>`;
+	});
 })
 
-$('#meat').on('change', function(e) {
-	$('#meat').children().each((i, option) => {
-		if (option.selected === true) {
-			selectedMeat = $(option).val();
-		}
-	})
-	SandwichMaker.meat(selectedMeat);
-	selectedToppings += `<li>${selectedMeat}</li>`;
+$('.meat').each((i, option) => {
+	$(option).on('click', function() {
+		selectedMeat = $(option).val();
+		SandwichMaker.meat(selectedMeat);
+		selectedToppings += `<li>${selectedMeat}</li>`;
+	});
 })
 
-$('#cheese').on('change', function(e) {
-	$('#cheese').children().each((i, option) => {
-		if (option.selected === true) {
-			selectedCheese = $(option).val();
-		}
-	})
-	SandwichMaker.cheese(selectedCheese);
-	selectedToppings += `<li>${selectedCheese}</li>`;
+$('.cheese').each((i, option) => {
+	$(option).on('click', function() {
+		selectedCheese = $(option).val();
+		SandwichMaker.cheese(selectedCheese);
+		selectedToppings += `<li>${selectedCheese}</li>`;
+	});
 })
 
-$('#veggies').on('change', function(e) {
-	$('#veggies').children().each((i, option) => {
-		if (option.selected === true) {
-			selectedVeggie = $(option).val();
-		}
-	})
-	SandwichMaker.veggies(selectedVeggie);
-	selectedToppings += `<li>${selectedVeggie}</li>`;
+$('.veggies').each((i, option) => {
+	$(option).on('click', function() {
+		selectedVeggies = $(option).val();
+		SandwichMaker.veggies(selectedVeggies);
+		selectedToppings += `<li>${selectedVeggies}</li>`;
+	});
 })
 
-$('#condiments').on('change', function(e) {
-	$('#condiments').children().each((i, option) => {
-		if (option.selected === true) {
-			selectedCondiment = $(option).val();
-		}
-	})
-	SandwichMaker.condiments(selectedCondiment);
-	selectedToppings += `<li>${selectedCondiment}</li>`;
+$('.condiments').each((i, option) => {
+	$(option).on('click', function() {
+		selectedCondiment = $(option).val();
+		SandwichMaker.condiments(selectedCondiment);
+		selectedToppings += `<li>${selectedCondiment}</li>`;
+	});
 })
 
 $('#place-order').on('click', function () {
